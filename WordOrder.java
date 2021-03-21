36a949481b37f20188e82d24652096db56bbfde3
IvaSimeonova <72191568+IvaSimeonova@users.noreply.github.com>
2021-03-21 11:28:06 +0200

    Update WordOrder.java

--- WordOrder.java
+++ WordOrder.java
@@ -2,9 +2,27 @@ package Lists_Stack_Queue_PriorityQueue;
 
 public class WordOrder {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+  public static void main(String[] args) {
+  ArrayList<String> words = new ArrayLis<> ();
+  File myObj = new File("words.txt");
+				
+				try {
+					Scanner myReader = new Scanner(myObj);
+					while (myReader.hasNextLine()) {
+						String line = myReader.nextLine();
+            String lineWords[] = line.split("[\\s.,]+");
+            words.addAll(Arrays.asList(lineWords));
+					}
+					myReader.close();
+				} catch (FileNotFoundException e) {
+					try {
+						myObj.createNewFile();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+        Collections.sort(words);
+        System.out.println(words);
+  }
 }
